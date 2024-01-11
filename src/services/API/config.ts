0dd31f246d57f2dde5a7fcc67943e450bc456388
Gregor Žunič <36313686+gregpr07@users.noreply.github.com>
2024-01-11 18:41:39 +0100

    fixed the authentication error

--- src/services/API/config.ts
+++ src/services/API/config.ts
@@ -1,12 +1,13 @@
 import { gotScraping } from "got-scraping";
-import { CookieJar } from "tough-cookie"
+import { CookieJar } from "tough-cookie";
 
-export const BASE_URL = 'https://apptoogoodtogo.com/api/';
-export const API_ITEM_ENDPOINT = 'item/v8/';
-export const AUTH_BY_EMAIL_ENDPOINT = 'auth/v3/authByEmail';
-export const AUTH_POLLING_ENDPOINT = 'auth/v3/authByRequestPollingId';
-export const REFRESH_ENDPOINT = 'auth/v3/token/refresh';
-export const USER_AGENT = "TooGoodToGo/21.9.0 (813) (iPhone/iPhone 7 (GSM); iOS 15.1; Scale/2.00)";
+export const BASE_URL = "https://apptoogoodtogo.com/api/";
+export const API_ITEM_ENDPOINT = "item/v8/";
+export const AUTH_BY_EMAIL_ENDPOINT = "auth/v3/authByEmail";
+export const AUTH_POLLING_ENDPOINT = "auth/v3/authByRequestPollingId";
+export const REFRESH_ENDPOINT = "auth/v3/token/refresh";
+export const USER_AGENT =
+  "TooGoodToGo/23.12.1 (19316) (iPhone/iPhone 13; iOS 17.2.1; Scale/3.00/iOS)";
 export const DEVICE_TYPE = "IOS";
 
 const cookieJar = new CookieJar();
@@ -29,9 +30,7 @@ export const api = gotScraping.extend({
   },
   // @ts-ignore
   headerGeneratorOptions: {
-    browsers: [
-      { name: "safari" },
-    ],
+    browsers: [{ name: "safari" }],
     devices: ["mobile"],
     locales: ["en-US"],
     operatingSystems: ["ios"],
